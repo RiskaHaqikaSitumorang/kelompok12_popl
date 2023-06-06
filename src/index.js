10eabbcc41ae50deb88d187888609a6dd100686e
iambrijeshtoo <chavdabrijeshkumar25@gmail.com>
2023-06-06 14:37:29 +0530

    feat: add react component and render it

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { ReactDOM } from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Component
+// - Component == JavaScript function
+// - React component are JavaScript function.
+// - Use PascalCase for naming component.
+// - Must return JSX (HTML)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default function Component() {
+  return <h1>I am Component</h1>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Component />);
