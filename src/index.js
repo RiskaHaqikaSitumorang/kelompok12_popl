2218d75388577323c1ae2c1624b93b5539da65c1
iambrijeshtoo <chavdabrijeshkumar25@gmail.com>
2023-06-06 17:52:26 +0530

    feat: add nested component

--- src/index.js
+++ src/index.js
@@ -8,8 +8,17 @@ import ReactDOM from "react-dom/client";
 // - Must return JSX (HTML)
 
 export default function Component() {
-  return <h1>I am Component</h1>;
+  return (
+    <div>
+      <NestedComponent />
+      <h1>I am Component</h1>;
+    </div>
+  );
 }
+
+// Nested Component
+// - To nest components you need to create first one parent element and then nest components how many you want.
+const NestedComponent = () => <h1>I am nested component</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
