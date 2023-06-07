46e45c6b88f6c941e897e6c9631cfb7247d7d3bb
iambrijeshtoo <chavdabrijeshkumar25@gmail.com>
2023-06-07 20:02:32 +0530

    feat: add componetns for book list

--- src/index.js
+++ src/index.js
@@ -1,25 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// Component
-// - Component == JavaScript function
-// - React component are JavaScript function.
-// - Use PascalCase for naming component.
-// - Must return JSX (HTML)
-
-export default function Component() {
+const BookList = () => {
   return (
-    <div>
-      <NestedComponent />
-      <h1>I am Component</h1>;
-    </div>
+    <section>
+      <Book />
+    </section>
   );
-}
+};
 
-// Nested Component
-// - To nest components you need to create first one parent element and then nest components how many you want.
-const NestedComponent = () => <h1>I am nested component</h1>;
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => <h2>Image Placeholder</h2>;
+const Title = () => <h2>Book Title</h2>;
+const Author = () => <h4>Author</h4>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Component />);
+root.render(<BookList />);
