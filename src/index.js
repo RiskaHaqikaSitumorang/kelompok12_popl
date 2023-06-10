e5aead6571fd350081c926360876a57fc9fb1a4d
iambrijeshtoo <chavdabrijeshkumar25@gmail.com>
2023-06-10 21:40:56 +0530

    feat: add map method to destructring books array
    - use map method to destructring books array and getting one element at
      the time and passing them into book component.

--- src/index.js
+++ src/index.js
@@ -3,40 +3,33 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
-const firstBook = {
-  author: "Francesc Miralles and Hector Garcia",
-  title: "Ikigai: The Japanese secret to a long and happy life",
-  image:
-    "https://m.media-amazon.com/images/I/51xwGSNX-EL._SX356_BO1,204,203,200_.jpg",
-};
-
-const secondBook = {
-  author: " Napoleon Hill",
-  title: "Think and Grow Rich",
-  image:
-    "https://m.media-amazon.com/images/I/41CqlzJS-NL._SX322_BO1,204,203,200_.jpg",
-};
+const books = [
+  {
+    author: "Francesc Miralles and Hector Garcia",
+    title: "Ikigai: The Japanese secret to a long and happy life",
+    image:
+      "https://m.media-amazon.com/images/I/51xwGSNX-EL._SX356_BO1,204,203,200_.jpg",
+  },
+  {
+    author: " Napoleon Hill",
+    title: "Think and Grow Rich",
+    image:
+      "https://m.media-amazon.com/images/I/41CqlzJS-NL._SX322_BO1,204,203,200_.jpg",
+  },
+];
 
 const BookList = () => {
   return (
     <section className="BookList">
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        image={firstBook.image}
-      />
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        image={secondBook.image}
-      />
+      {books.map((books) => {
+        const { author, title, image } = books;
+        return <Book author={author} title={title} image={image} />;
+      })}
     </section>
   );
 };
 
-const Book = (props) => {
-  const { author, title, image } = props;
-
+const Book = ({ author, title, image }) => {
   return (
     <article className="Book">
       <img src={image} alt={title} />
